536fb9174bdd55f89cd4cc503959733cdc178aa1
Abdul Khan <akhan17@outlook.com>
2020-05-08 22:24:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,16 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Maru", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "M", "BROWN", "HAPPY", True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "M", "BROWN", "HAPPY", True);
+
+
+
+
+
+
+
+NULL
